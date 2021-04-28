62bf9bde2c1553c04be1ecce2de7a5e5e8a8264b
stareta1202 <stareta1202@gmail.com>
2021-04-28 22:03:37 +0900

    starbucks crawling

--- first.py
+++ first.py
@@ -1,51 +1,29 @@
-import requests
+import csv
+import re
+import time
+
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+filename = "new2starbucks_product.csv"
+csv_open = open(filename, "w+", encoding="utf-8")
+csv_writer = csv.writer(csv_open)
+
+driver = webdriver.Chrome("/Users/yongjun/.wdm/drivers/chromedriver/mac64/90.0.4430.24/chromedriver")
+url = "https://www.starbucks.co.kr/menu/drink_list.do"
+driver.get(url)
+time.sleep(7)
 
 
-def print_stock_price(code, page_num):
-    result = [[], [], [], [], [], [], [], [], []]
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+drinks = soup.findAll("li", {"class": re.compile("menuDataSet")})
 
-    for n in range(page_num):
-        url = 'https://finance.naver.com/item/sise_day.nhn?code=' + code + '&page=' + str(n + 1)
-        print(url)
+for drink in drinks:
+    image_tag = drink.find("img")
+    image_url = image_tag['src']
+    title = image_tag['alt']
+    csv_writer.writerow((title, image_url))
 
-        r = requests.get(url)
-
-        if not r.ok:
-            print('Not more data !')
-            break
-
-        html = r.content
-        soup = BeautifulSoup(html, 'html.parser')
-        tr = soup.select('table > tr')
-
-        for i in range(1, len(tr) - 1):
-            td = tr[i].select('td')
-            if td[0].text.strip():
-                result[0].append(td[0].text.strip())  # 날짜
-                result[1].append(td[1].text.strip())  # 종가
-
-                img = td[2].select('img')
-                if len(img) != 0:
-                    if 'src' in img[0].attrs:
-                        src = img[0]['src']
-                        if 'up' in src:
-                            result[2].append('상승')
-                        else:
-                            result[2].append('하락')
-                else:
-                    result[2].append('보합')
-
-                result[3].append(td[2].text.strip())  # 전일대비
-                result[4].append(td[3].text.strip())  # 시장가
-                result[5].append(td[4].text.strip())  # 최고가
-                result[6].append(td[5].text.strip())  # 최저가
-                result[7].append(td[6].text.strip())  # 거래량
-
-    for i in range(len(result[0])):
-        #     날짜          종가           상승/하락/보합+a           시장가         최고가        최저가        거래량
-        print(result[0][i], result[1][i], result[2][i] + result[3][i], result[4][i], result[5][i], result[6][i],
-              result[7][i])
-
-
-print_stock_price(code='005930', page_num=1)
+csv_open.close()
